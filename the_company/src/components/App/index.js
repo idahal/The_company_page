e36fcf68c37c4aa7f316f59a1cf951f9699f5f8f
Ida Hallner <bee_ida@hotmail.com>
2019-08-07 20:25:33 +0200

    import hero component

--- the_company/src/components/App/index.js
+++ the_company/src/components/App/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import Footer from '../Footer';
+import Hero from '../Hero';
 import instagram from './instagram-brands.svg';
 
 function App() {
   return (
     <div className="App">
-          <Footer> 
-            <p>Adress: Karis Lycka 6 433 51 Öjersjö</p>
-            <p>Telefon: 031-7619101</p>
-            <p>Email: info@byggvolt.se</p>
-            <img src={instagram} alt="instagram-brands-logo" />
-          </Footer>
+        <Hero />
+        <Footer> 
+          <p>Adress: Karis Lycka 6 433 51 Öjersjö</p>
+          <p>Telefon: 031-7619101</p>
+          <p>Email: info@byggvolt.se</p>
+          <img src={instagram} alt="instagram-brands-logo" />
+        </Footer>
     </div>
   );
 }
