e01284a350a376266936d2a2f0c240706cd62814
Ida Hallner <bee_ida@hotmail.com>
2019-08-07 19:00:14 +0200

    add correct footer info

--- the_company/src/components/App/index.js
+++ the_company/src/components/App/index.js
@@ -5,7 +5,11 @@ import Footer from '../Footer';
 function App() {
   return (
     <div className="App">
-      <Footer/>
+          <Footer> 
+            <p>Adress: Karis Lycka 6 433 51 Öjersjö</p>
+            <p>Telefon: 031-7619101</p>
+            <p>Email: info@byggvolt.se</p>
+          </Footer>
     </div>
   );
 }
