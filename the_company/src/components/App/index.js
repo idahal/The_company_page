26816370b31c42284397a8a9617498fb19e5e76a
Ida Hallner <bee_ida@hotmail.com>
2019-08-08 18:07:54 +0200

    import box component

--- the_company/src/components/App/index.js
+++ the_company/src/components/App/index.js
@@ -3,6 +3,8 @@ import './App.css';
 import Footer from '../Footer';
 import Hero from '../Hero';
 import About from '../About';
+import Box from '../Box';
+
 
 import instagram from './instagram-brands.svg';
 
@@ -11,6 +13,16 @@ function App() {
     <div className="App">
         <Hero />
         <About />
+        <Box>
+        <p>Trygghet är viktigt.</p>
+        <h1>Anlita oss</h1>
+        <h3>
+          Att anlita oss ska ge dig som kund en positiv upplevelse inte bara i själva utförandet utan också i den rent personliga kontakten.
+          Vi hoppas att vår positiva inställning ska ge dig som kund ett mervärde även i din egen vardag. 
+        </h3>
+          
+        </Box>
+ 
         <Footer> 
           <p>Adress: Karis Lycka 6 433 51 Öjersjö</p>
           <p>Telefon: 031-7619101</p>
