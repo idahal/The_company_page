6685d212dc161e923b3c1679d157b651b4e17392
Ida Hallner <bee_ida@hotmail.com>
2019-08-08 18:07:28 +0200

    create box component

---
+++ the_company/src/components/Box/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from "styled-components";
+
+const StyledBox = styled.div`
+    width: 100%;
+    background-color: rgb(230, 138, 0); 
+    color: rgb(255, 255, 255);
+    padding: 1%;
+    margin: 5% 0 5% 0;
+    height: 300px;
+    h1 {
+        font-size: 2rem;
+        text-transform: uppercase;
+    }
+
+    h3 {
+        width: 80%;
+        font-size: 1.2rem;
+        text-align: left;
+    }
+    p {
+        text-transform: uppercase;
+    }
+
+@media screen and (max-width: 800px) {
+    padding: 1% 5% 1% 5%;
+
+    h1 {
+
+    }
+    h3 {
+        width: 100%;
+        font-size: 0.9rem;
+
+    }
+ `;
+
+const Box = props => {
+    return (
+      <StyledBox>
+           {props.children}
+      </StyledBox>
+    )
+  }
+
+export default Box;
