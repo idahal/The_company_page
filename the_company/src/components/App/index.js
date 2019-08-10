efeba3282f2dbbd83f9d9f01c963d70abf7ef3c7
Ida Hallner <bee_ida@hotmail.com>
2019-08-10 18:10:49 +0200

    import navbar

--- the_company/src/components/App/index.js
+++ the_company/src/components/App/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Router } from "@reach/router";
 import Footer from '../Footer';
 import Home from '../Home';
+import Navbar from '../Navbar';
 import instagram from './instagram-brands.svg';
 
 function App() {
   return (
     <div className="App">
-      <Home />  
+      <Navbar />
+      <Router>
+        <Home path="/" />
+      </Router> 
       <Footer> 
         <p>Adress: Karis Lycka 6 433 51 Öjersjö</p>
         <p>Telefon: 031-7619101</p>
