b1bf4405f684268b3627ff38c04717566b0a474e
Ida Hallner <bee_ida@hotmail.com>
2019-08-16 10:19:48 +0200

    create data for employees

---
+++ the_company/src/data/employees.js
@@ -0,0 +1,19 @@
+import workerOne from '../../src/images/krister.PNG';
+import workerTwo from '../../src/images/hans.PNG';
+import workerThree from '../../src/images/andreas.PNG';
+
+const employees = [  {
+    image: workerOne,
+    description: "krister@byggolt.se"    
+},
+{
+    image: workerTwo,
+    description: "hans@byggolt.se" 
+},
+{
+    image: workerThree,
+    description: "andreas@byggolt.se" 
+}
+]
+
+export default employees;
