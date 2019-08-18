fb71cc8eccf78d0030f54c531cf5941e1c05b4c3
Ida Hallner <bee_ida@hotmail.com>
2019-08-18 13:06:00 +0200

    change % to em

--- the_company/src/components/Box/index.js
+++ the_company/src/components/Box/index.js
@@ -5,8 +5,8 @@ const StyledBox = styled.div`
     width: 100%;
     background-color: rgb(230, 138, 0); 
     color: rgb(255, 255, 255);
-    padding: 1%;
-    margin: 2% 0 2% 0;
+    padding: 1em;
+    margin: 1em 0 1em 0;
     height: 300px;
     
     h1 {
@@ -31,9 +31,8 @@ const StyledBox = styled.div`
 
 @media screen and (max-width: 800px) {
     height: 270px;
-    margin: 2% 0 3% 0;
-    padding: 1% 5% 1% 5%;
-
+    margin: 1em 0 1em 0;
+    
     h3 {
         width: 100%;
         font-size: 0.9rem;
